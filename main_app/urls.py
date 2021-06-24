6e25a74e63c8e9a9833d9c42e9f7db3f88ca0f48
Rome Bell <romebell5@gmail.com>
2021-06-24 06:45:32 -0700

    add url path for user

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
+    path('user/<username>/', views.profile, name='profile'),
 ]
 
 # When thinking about making a webpage inside of Django
